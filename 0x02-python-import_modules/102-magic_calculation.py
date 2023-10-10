e0240ba57c98ed2259af35422079a3c4e688454b
keltoumay <keltoumaouyahia@gmail.com>
2023-10-10 13:35:35 -0700

    update

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+#102-magic_calculation.pyin/python3
 
 
 def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
+
     from magic_calculation_102 import add, sub
 
     if a < b:
