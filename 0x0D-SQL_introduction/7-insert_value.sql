c4aae9f2e9223e891952e2ee0f850cd59b024c12
keltoumy <keltoumaouyahia@gmail.com>
2023-12-13 14:13:27 -0800

    First add

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- Inserts new row into `first_table`, DB passed as script arg.
-INSERT INTO first_table (id, name)
-VALUES (89, 'Holberton School');
+-- script that inserts a new row in the table first_table (database
+-- hbtn_0c_0) in your MySQL server.
+INSERT INTO first_table (id, name) VALUES (89, "Best School");
